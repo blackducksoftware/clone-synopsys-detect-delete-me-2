blackducksoftware/synopsys-detect

Adding bitbake(yocto/poky) support

hub-detect/src/main/groovy/com/blackducksoftware/integration/hub/detect/BeanConfiguration.java
@@ -40,14 +40,16 @@ import com.blackducksoftware.integration.hub.bdio.BdioTransformer;
 import com.blackducksoftware.integration.hub.bdio.SimpleBdioFactory;
 import com.blackducksoftware.integration.hub.bdio.graph.DependencyGraphTransformer;
 import com.blackducksoftware.integration.hub.bdio.model.externalid.ExternalIdFactory;
+import com.blackducksoftware.integration.hub.detect.bomtool.bitbake.BitbakeExtractor;
+import com.blackducksoftware.integration.hub.detect.bomtool.bitbake.GraphParserTransformer;
 import com.blackducksoftware.integration.hub.detect.bomtool.clang.ApkPackageManager;
+import com.blackducksoftware.integration.hub.detect.bomtool.clang.ClangExtractor;
+import com.blackducksoftware.integration.hub.detect.bomtool.clang.CodeLocationAssembler;
 import com.blackducksoftware.integration.hub.detect.bomtool.clang.CompileCommandsJsonFileParser;
 import com.blackducksoftware.integration.hub.detect.bomtool.clang.DependenciesListFileManager;
-import com.blackducksoftware.integration.hub.detect.bomtool.clang.ClangExtractor;
-import com.blackducksoftware.integration.hub.detect.bomtool.clang.PackageManagerFinder;
-import com.blackducksoftware.integration.hub.detect.bomtool.clang.CodeLocationAssembler;
 import com.blackducksoftware.integration.hub.detect.bomtool.clang.DpkgPackageManager;
 import com.blackducksoftware.integration.hub.detect.bomtool.clang.LinuxPackageManager;
+import com.blackducksoftware.integration.hub.detect.bomtool.clang.PackageManagerFinder;
 import com.blackducksoftware.integration.hub.detect.bomtool.clang.RpmPackageManager;
 import com.blackducksoftware.integration.hub.detect.bomtool.cocoapods.PodlockExtractor;
 import com.blackducksoftware.integration.hub.detect.bomtool.cocoapods.PodlockParser;
@@ -324,11 +326,10 @@ public class BeanConfiguration {
 
     @Bean
     public BomToolFactory bomToolFactory() throws ParserConfigurationException {
-        return new BomToolFactory(detectConfigWrapper(), detectFileFinder(), standardExecutableFinder(), cLangExtractor(), cLangPackageManagerFinder(), composerLockExtractor(), condaCliExtractor(), cpanCliExtractor(), dockerExtractor(),
-                dockerInspectorManager(),
-                gemlockExtractor(), goDepExtractor(), goInspectorManager(), goVndrExtractor(), gradleExecutableFinder(), gradleInspectorExtractor(), gradleInspectorManager(), mavenCliExtractor(), mavenExecutableFinder(), npmCliExtractor(),
-                npmExecutableFinder(), npmLockfileExtractor(), nugetInspectorExtractor(), nugetInspectorManager(), packratLockExtractor(), pearCliExtractor(), pipInspectorExtractor(), pipInspectorManager(), pipenvExtractor(),
-                podlockExtractor(), pythonExecutableFinder(), rebarExtractor(), sbtResolutionCacheExtractor(), yarnLockExtractor());
+        return new BomToolFactory(detectConfigWrapper(), detectFileFinder(), standardExecutableFinder(), bitbakeExtractor(), cLangExtractor(), cLangPackageManagerFinder(), composerLockExtractor(), condaCliExtractor(), cpanCliExtractor(),
+            dockerExtractor(), dockerInspectorManager(), gemlockExtractor(), goDepExtractor(), goInspectorManager(), goVndrExtractor(), gradleExecutableFinder(), gradleInspectorExtractor(), gradleInspectorManager(), mavenCliExtractor(),
+            mavenExecutableFinder(), npmCliExtractor(), npmExecutableFinder(), npmLockfileExtractor(), nugetInspectorExtractor(), nugetInspectorManager(), packratLockExtractor(), pearCliExtractor(), pipInspectorExtractor(),
+            pipInspectorManager(), pipenvExtractor(), podlockExtractor(), pythonExecutableFinder(), rebarExtractor(), sbtResolutionCacheExtractor(), yarnLockExtractor());
     }
 
     @Bean
@@ -433,6 +434,16 @@ public class BeanConfiguration {
     @Bean
     public CodeLocationAssembler codeLocationAssembler() {
         return new CodeLocationAssembler(externalIdFactory());
+    }
+
+    @Bean
+    public BitbakeExtractor bitbakeExtractor() {
+        return new BitbakeExtractor(executableManager(), executableRunner(), detectConfigWrapper(), detectFileManager(), detectFileFinder(), graphTransformer());
+    }
+
+    @Bean
+    public GraphParserTransformer graphTransformer() {
+        return new GraphParserTransformer();
     }
 
     @Bean

hub-detect/src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/bitbake/BitbakeBomTool.java
@@ -1,0 +1,91 @@
+/**
+ * hub-detect
+ *
+ * Copyright (C) 2018 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.blackducksoftware.integration.hub.detect.bomtool.bitbake;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.blackducksoftware.integration.hub.bdio.model.Forge;
+import com.blackducksoftware.integration.hub.detect.bomtool.BomTool;
+import com.blackducksoftware.integration.hub.detect.bomtool.BomToolEnvironment;
+import com.blackducksoftware.integration.hub.detect.bomtool.BomToolGroupType;
+import com.blackducksoftware.integration.hub.detect.bomtool.BomToolType;
+import com.blackducksoftware.integration.hub.detect.bomtool.ExtractionId;
+import com.blackducksoftware.integration.hub.detect.configuration.DetectConfigWrapper;
+import com.blackducksoftware.integration.hub.detect.configuration.DetectProperty;
+import com.blackducksoftware.integration.hub.detect.util.DetectFileFinder;
+import com.blackducksoftware.integration.hub.detect.util.executable.ExecutableRunnerException;
+import com.blackducksoftware.integration.hub.detect.workflow.bomtool.BomToolResult;
+import com.blackducksoftware.integration.hub.detect.workflow.bomtool.FileNotFoundBomToolResult;
+import com.blackducksoftware.integration.hub.detect.workflow.bomtool.PassedBomToolResult;
+import com.blackducksoftware.integration.hub.detect.workflow.bomtool.PropertyInsufficientBomToolResult;
+import com.blackducksoftware.integration.hub.detect.workflow.extraction.Extraction;
+
+public class BitbakeBomTool extends BomTool {
+    public static Forge YOCTO_FORGE = new Forge("/", "/", "yocto");
+
+    private final DetectFileFinder detectFileFinder;
+    private final DetectConfigWrapper detectConfigWrapper;
+    private final BitbakeExtractor bitbakeExtractor;
+
+    private File foundBuildEnvScript;
+
+    public BitbakeBomTool(final BomToolEnvironment bomToolEnvironment, final DetectFileFinder detectFileFinder, final DetectConfigWrapper detectConfigWrapper, final BitbakeExtractor bitbakeExtractor) {
+        super(bomToolEnvironment, "Bitbake", BomToolGroupType.BITBAKE, BomToolType.BITBAKE_CLI);
+        this.detectFileFinder = detectFileFinder;
+        this.detectConfigWrapper = detectConfigWrapper;
+        this.bitbakeExtractor = bitbakeExtractor;
+    }
+
+    @Override
+    public BomToolResult applicable() {
+        foundBuildEnvScript = detectFileFinder.findFile(environment.getDirectory(), detectConfigWrapper.getProperty(DetectProperty.DETECT_INIT_BUILD_ENV_NAME));
+        if (foundBuildEnvScript == null) {
+            return new FileNotFoundBomToolResult(DetectProperty.DETECT_INIT_BUILD_ENV_NAME.getDefaultValue());
+        }
+
+        return new PassedBomToolResult();
+    }
+
+    @Override
+    public BomToolResult extractable() {
+        final String packageName = detectConfigWrapper.getProperty(DetectProperty.DETECT_BITBAKE_PACKAGE_NAME);
+        if (StringUtils.isBlank(packageName)) {
+            return new PropertyInsufficientBomToolResult();
+        }
+
+        return new PassedBomToolResult();
+    }
+
+    @Override
+    public Extraction extract(final ExtractionId extractionId) {
+        try {
+            return bitbakeExtractor.extract(extractionId, foundBuildEnvScript.getCanonicalPath(), environment.getDirectory().getCanonicalPath());
+        } catch (final IOException | ExecutableRunnerException e) {
+            return new Extraction.Builder().failure(String.format("Failed to extract dependencies from bitbake: %s", e.getMessage())).build();
+        }
+    }
+}

hub-detect/src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/bitbake/GraphParserTransformer.java
@@ -1,0 +1,97 @@
+/**
+ * hub-detect
+ *
+ * Copyright (C) 2018 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.blackducksoftware.integration.hub.detect.bomtool.bitbake;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.blackducksoftware.integration.hub.bdio.graph.DependencyGraph;
+import com.blackducksoftware.integration.hub.bdio.graph.builder.LazyExternalIdDependencyGraphBuilder;
+import com.blackducksoftware.integration.hub.bdio.model.dependencyid.DependencyId;
+import com.blackducksoftware.integration.hub.bdio.model.dependencyid.NameDependencyId;
+import com.blackducksoftware.integration.hub.bdio.model.externalid.ExternalId;
+import com.paypal.digraph.parser.GraphEdge;
+import com.paypal.digraph.parser.GraphNode;
+import com.paypal.digraph.parser.GraphParser;
+
+public class GraphParserTransformer {
+    public DependencyGraph transform(final GraphParser graphParser) {
+        final Map<String, GraphNode> nodes = graphParser.getNodes();
+        final Map<String, GraphEdge> edges = graphParser.getEdges();
+        final LazyExternalIdDependencyGraphBuilder graphBuilder = new LazyExternalIdDependencyGraphBuilder();
+
+        for (final GraphNode graphNode : nodes.values()) {
+            final String name = getNameFromNode(graphNode);
+            final DependencyId dependencyId = new NameDependencyId(name);
+            final Optional<String> version = getVersionFromNode(graphNode);
+
+            if (version.isPresent()) {
+                final ExternalId externalId = new ExternalId(BitbakeBomTool.YOCTO_FORGE);
+                externalId.name = name;
+                externalId.version = version.get();
+                graphBuilder.setDependencyInfo(dependencyId, name, version.get(), externalId);
+            }
+
+            graphBuilder.addChildToRoot(dependencyId);
+        }
+
+        for (final GraphEdge graphEdge : edges.values()) {
+            final DependencyId node1 = new NameDependencyId(getNameFromNode(graphEdge.getNode1()));
+            final DependencyId node2 = new NameDependencyId(getNameFromNode(graphEdge.getNode2()));
+            graphBuilder.addParentWithChild(node1, node2);
+        }
+
+        return graphBuilder.build();
+    }
+
+    private String getNameFromNode(final GraphNode graphNode) {
+        return graphNode.getId().replaceAll("\"", "");
+    }
+
+    private Optional<String> getVersionFromNode(final GraphNode graphNode) {
+        final Optional<String> attribute = getLabelAttribute(graphNode);
+
+        return attribute.map(this::getVersionFromLabel);
+    }
+
+    private Optional<String> getLabelAttribute(final GraphNode graphNode) {
+        final String attribute = (String) graphNode.getAttribute("label");
+        Optional<String> result = Optional.empty();
+
+        if (StringUtils.isNotBlank(attribute)) {
+            result = Optional.of(attribute);
+        }
+
+        return result;
+    }
+
+    private String getVersionFromLabel(final String label) {
+        final String[] components = label.split("\\\\n:|\\\\n");
+        final String version = components[1];
+
+        return version;
+    }
+}

hub-detect/src/test/groovy/com/blackducksoftware/integration/hub/detect/bomtool/bitbake/GraphParserTransformerTest.java
@@ -1,0 +1,22 @@
+package com.blackducksoftware.integration.hub.detect.bomtool.bitbake;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import com.blackducksoftware.integration.hub.bdio.graph.DependencyGraph;
+import com.paypal.digraph.parser.GraphParser;
+
+public class GraphParserTransformerTest {
+    @Test
+    public void transform() throws IOException {
+        final GraphParserTransformer graphParserTransformer = new GraphParserTransformer();
+        final InputStream inputStream = new ClassPathResource("/bitbake/recipe-depends.dot").getInputStream();
+        final GraphParser graphParser = new GraphParser(inputStream);
+        final DependencyGraph dependencyGraph = graphParserTransformer.transform(graphParser);
+
+        assert dependencyGraph.getRootDependencies().size() == 480;
+    }
+}

blackducksoftware/clone-synopsys-detect-delete-me-2

hub-detect/src/main/groovy/com/blackducksoftware/integration/hub/detect/BeanConfiguration.java
@@ -34,6 +34,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+import com.blackducksoftware.integration.hub.detect.bomtool.bitbake.BitbakeExtractor;
+import com.blackducksoftware.integration.hub.detect.bomtool.bitbake.GraphParserTransformer;
 import com.blackducksoftware.integration.hub.detect.bomtool.clang.ApkPackageManager;
 import com.blackducksoftware.integration.hub.detect.bomtool.clang.ClangExtractor;
 import com.blackducksoftware.integration.hub.detect.bomtool.clang.ClangLinuxPackageManager;
@@ -359,13 +361,21 @@ public class BeanConfiguration {
     }
 
     @Bean
+    public GraphParserTransformer graphParserTransformer() {
+        return new GraphParserTransformer();
+    }
+
+    @Bean
+    public BitbakeExtractor bitbakeExtractor() {
+        return new BitbakeExtractor(executableManager(), executableRunner(), detectConfiguration(), detectFileManager(), detectFileFinder(), graphParserTransformer());
+    }
+
+    @Bean
     public BomToolFactory bomToolFactory() throws ParserConfigurationException {
-        return new BomToolFactory(detectConfiguration(), detectFileFinder(), standardExecutableFinder(), executableRunner(), clangExtractor(), clangLinuxPackageManagers(), composerLockExtractor(), condaCliExtractor(), cpanCliExtractor(),
-            dockerExtractor(),
-            dockerInspectorManager(),
-            gemlockExtractor(), goDepExtractor(), goInspectorManager(), goVndrExtractor(), gradleExecutableFinder(), gradleInspectorExtractor(), gradleInspectorManager(), mavenCliExtractor(), mavenExecutableFinder(), npmCliExtractor(),
-            npmExecutableFinder(), npmLockfileExtractor(), nugetInspectorExtractor(), nugetInspectorManager(), packratLockExtractor(), pearCliExtractor(), pipInspectorExtractor(), pipInspectorManager(), pipenvExtractor(),
-            podlockExtractor(), pythonExecutableFinder(), rebarExtractor(), sbtResolutionCacheExtractor(), yarnLockExtractor());
+        return new BomToolFactory(detectConfiguration(), detectFileFinder(), standardExecutableFinder(), executableRunner(), bitbakeExtractor(), clangExtractor(), clangLinuxPackageManagers(), composerLockExtractor(), condaCliExtractor(),
+            cpanCliExtractor(), dockerExtractor(), dockerInspectorManager(), gemlockExtractor(), goDepExtractor(), goInspectorManager(), goVndrExtractor(), gradleExecutableFinder(), gradleInspectorExtractor(), gradleInspectorManager(),
+            mavenCliExtractor(), mavenExecutableFinder(), npmCliExtractor(), npmExecutableFinder(), npmLockfileExtractor(), nugetInspectorExtractor(), nugetInspectorManager(), packratLockExtractor(), pearCliExtractor(),
+            pipInspectorExtractor(), pipInspectorManager(), pipenvExtractor(), podlockExtractor(), pythonExecutableFinder(), rebarExtractor(), sbtResolutionCacheExtractor(), yarnLockExtractor());
     }
 
     @Bean

hub-detect/src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/bitbake/BitbakeBomTool.java
@@ -1,0 +1,91 @@
+/**
+ * hub-detect
+ *
+ * Copyright (C) 2018 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.blackducksoftware.integration.hub.detect.bomtool.bitbake;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.blackducksoftware.integration.hub.detect.bomtool.BomTool;
+import com.blackducksoftware.integration.hub.detect.bomtool.BomToolEnvironment;
+import com.blackducksoftware.integration.hub.detect.bomtool.BomToolGroupType;
+import com.blackducksoftware.integration.hub.detect.bomtool.BomToolType;
+import com.blackducksoftware.integration.hub.detect.bomtool.ExtractionId;
+import com.blackducksoftware.integration.hub.detect.configuration.DetectConfiguration;
+import com.blackducksoftware.integration.hub.detect.configuration.DetectProperty;
+import com.blackducksoftware.integration.hub.detect.util.DetectFileFinder;
+import com.blackducksoftware.integration.hub.detect.util.executable.ExecutableRunnerException;
+import com.blackducksoftware.integration.hub.detect.workflow.bomtool.BomToolResult;
+import com.blackducksoftware.integration.hub.detect.workflow.bomtool.FileNotFoundBomToolResult;
+import com.blackducksoftware.integration.hub.detect.workflow.bomtool.PassedBomToolResult;
+import com.blackducksoftware.integration.hub.detect.workflow.bomtool.PropertyInsufficientBomToolResult;
+import com.blackducksoftware.integration.hub.detect.workflow.extraction.Extraction;
+import com.synopsys.integration.hub.bdio.model.Forge;
+
+public class BitbakeBomTool extends BomTool {
+    public static Forge YOCTO_FORGE = new Forge("/", "/", "yocto");
+
+    private final DetectFileFinder detectFileFinder;
+    private final DetectConfiguration detectConfiguration;
+    private final BitbakeExtractor bitbakeExtractor;
+
+    private File foundBuildEnvScript;
+
+    public BitbakeBomTool(final BomToolEnvironment bomToolEnvironment, final DetectFileFinder detectFileFinder, final DetectConfiguration detectConfiguration, final BitbakeExtractor bitbakeExtractor) {
+        super(bomToolEnvironment, "Bitbake", BomToolGroupType.BITBAKE, BomToolType.BITBAKE_CLI);
+        this.detectFileFinder = detectFileFinder;
+        this.detectConfiguration = detectConfiguration;
+        this.bitbakeExtractor = bitbakeExtractor;
+    }
+
+    @Override
+    public BomToolResult applicable() {
+        foundBuildEnvScript = detectFileFinder.findFile(environment.getDirectory(), detectConfiguration.getProperty(DetectProperty.DETECT_INIT_BUILD_ENV_NAME));
+        if (foundBuildEnvScript == null) {
+            return new FileNotFoundBomToolResult(DetectProperty.DETECT_INIT_BUILD_ENV_NAME.getDefaultValue());
+        }
+
+        return new PassedBomToolResult();
+    }
+
+    @Override
+    public BomToolResult extractable() {
+        final String packageName = detectConfiguration.getProperty(DetectProperty.DETECT_BITBAKE_PACKAGE_NAME);
+        if (StringUtils.isBlank(packageName)) {
+            return new PropertyInsufficientBomToolResult();
+        }
+
+        return new PassedBomToolResult();
+    }
+
+    @Override
+    public Extraction extract(final ExtractionId extractionId) {
+        try {
+            return bitbakeExtractor.extract(extractionId, foundBuildEnvScript.getCanonicalPath(), environment.getDirectory().getCanonicalPath());
+        } catch (final IOException | ExecutableRunnerException e) {
+            return new Extraction.Builder().failure(String.format("Failed to extract dependencies from bitbake: %s", e.getMessage())).build();
+        }
+    }
+}

hub-detect/src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/bitbake/GraphParserTransformer.java
@@ -1,0 +1,97 @@
+/**
+ * hub-detect
+ *
+ * Copyright (C) 2018 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.blackducksoftware.integration.hub.detect.bomtool.bitbake;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.paypal.digraph.parser.GraphEdge;
+import com.paypal.digraph.parser.GraphNode;
+import com.paypal.digraph.parser.GraphParser;
+import com.synopsys.integration.hub.bdio.graph.DependencyGraph;
+import com.synopsys.integration.hub.bdio.graph.builder.LazyExternalIdDependencyGraphBuilder;
+import com.synopsys.integration.hub.bdio.model.dependencyid.DependencyId;
+import com.synopsys.integration.hub.bdio.model.dependencyid.NameDependencyId;
+import com.synopsys.integration.hub.bdio.model.externalid.ExternalId;
+
+public class GraphParserTransformer {
+    public DependencyGraph transform(final GraphParser graphParser) {
+        final Map<String, GraphNode> nodes = graphParser.getNodes();
+        final Map<String, GraphEdge> edges = graphParser.getEdges();
+        final LazyExternalIdDependencyGraphBuilder graphBuilder = new LazyExternalIdDependencyGraphBuilder();
+
+        for (final GraphNode graphNode : nodes.values()) {
+            final String name = getNameFromNode(graphNode);
+            final DependencyId dependencyId = new NameDependencyId(name);
+            final Optional<String> version = getVersionFromNode(graphNode);
+
+            if (version.isPresent()) {
+                final ExternalId externalId = new ExternalId(BitbakeBomTool.YOCTO_FORGE);
+                externalId.name = name;
+                externalId.version = version.get();
+                graphBuilder.setDependencyInfo(dependencyId, name, version.get(), externalId);
+            }
+
+            graphBuilder.addChildToRoot(dependencyId);
+        }
+
+        for (final GraphEdge graphEdge : edges.values()) {
+            final DependencyId node1 = new NameDependencyId(getNameFromNode(graphEdge.getNode1()));
+            final DependencyId node2 = new NameDependencyId(getNameFromNode(graphEdge.getNode2()));
+            graphBuilder.addParentWithChild(node1, node2);
+        }
+
+        return graphBuilder.build();
+    }
+
+    private String getNameFromNode(final GraphNode graphNode) {
+        return graphNode.getId().replaceAll("\"", "");
+    }
+
+    private Optional<String> getVersionFromNode(final GraphNode graphNode) {
+        final Optional<String> attribute = getLabelAttribute(graphNode);
+
+        return attribute.map(this::getVersionFromLabel);
+    }
+
+    private Optional<String> getLabelAttribute(final GraphNode graphNode) {
+        final String attribute = (String) graphNode.getAttribute("label");
+        Optional<String> result = Optional.empty();
+
+        if (StringUtils.isNotBlank(attribute)) {
+            result = Optional.of(attribute);
+        }
+
+        return result;
+    }
+
+    private String getVersionFromLabel(final String label) {
+        final String[] components = label.split("\\\\n:|\\\\n");
+        final String version = components[1];
+
+        return version;
+    }
+}

hub-detect/src/test/groovy/com/blackducksoftware/integration/hub/detect/bomtool/bitbake/GraphParserTransformerTest.java
@@ -1,0 +1,22 @@
+package com.blackducksoftware.integration.hub.detect.bomtool.bitbake;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import com.paypal.digraph.parser.GraphParser;
+import com.synopsys.integration.hub.bdio.graph.DependencyGraph;
+
+public class GraphParserTransformerTest {
+    @Test
+    public void transform() throws IOException {
+        final GraphParserTransformer graphParserTransformer = new GraphParserTransformer();
+        final InputStream inputStream = new ClassPathResource("/bitbake/recipe-depends.dot").getInputStream();
+        final GraphParser graphParser = new GraphParser(inputStream);
+        final DependencyGraph dependencyGraph = graphParserTransformer.transform(graphParser);
+
+        assert dependencyGraph.getRootDependencies().size() == 480;
+    }
+}